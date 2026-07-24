298ea5a28f357a8d168ac55e57362040ec7c8915
AbhinashBalaji <abhinashb1011@gmail.com>
2026-07-25 05:15:00 +0530

    Fix resend version

--- athenaiq/frontend/src/api.js
+++ athenaiq/frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_BASE = `http://${window.location.hostname}:8000`
+export const API_BASE = 'https://athenaiq-backend.onrender.com'
 
 const api = axios.create({ baseURL: API_BASE })
 
